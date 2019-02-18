e0cf74dd37eda4dcaa4c2363896ef8ed10d4e22e
Matt Smith <smithryg@yahoo.com>
2019-02-18 18:11:17 -0500

    commit#4
    new files & successful test
    
    -need to add CSS file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
 
+//localhost test & app code [6/18/2019]
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
