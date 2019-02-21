a41aa3d5e0bc04650c4e472a0cd3fc9d4287b5cb
Matt Smith <smithryg@yahoo.com>
2019-02-20 20:16:33 -0500

    added CSS & semicolons

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-app.set('view engine', 'ejs')
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 //localhost test & app code [6/18/2019]
 app.get('/', function (req, res) {
   res.render('index')
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
-
+});
